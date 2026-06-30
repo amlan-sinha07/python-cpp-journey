d01c6a196dfd5da5ab4a34e978e0758fa277ded1
Amlan Sinha <amlansinha386@gmail.com>
2026-07-01 00:07:24 +0530

    day 35 : leetcode 169,383

--- day35.cpp
+++ day35.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <string>
 
 using namespace std ;
 class Solution169 {
@@ -19,4 +20,24 @@ public:
         }return 0;
     } 
     
+};
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map <char ,int > seen ;
+        for (char x: ransomNote){
+            seen[x]++ ;
+        }
+        for (char x: magazine){
+            seen[x]-- ;
+        }
+        for (auto& pair: seen){
+            if (pair.second > 0){
+                return false;
+            }
+            
+            
+        }
+        return true;
+    }
 };
